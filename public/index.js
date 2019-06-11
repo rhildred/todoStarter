ece6784c99833652f4e863286dcdcbf5b81dbd50
rhildred <rhildred@gmail.com>
2019-06-11 13:50:27 -0400

    changing to todo app

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
-// Librairies
+// Libraries
 import Framework7 from 'framework7/framework7.esm'
+import config from "./firebase.js";
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 // Theme
 var theme = 'auto';
@@ -14,9 +17,14 @@ var app = new Framework7({
   },
 })
 
-document.getElementById("magic").addEventListener("submit", (evt)=>{
+function addTodo(sTodo){
+  let oTodo = document.createElement("p");
+  oTodo.innerHTML = sTodo;
+  document.getElementById("list").prepend(oTodo);
+}
+
+document.getElementById("todo").addEventListener("submit", (evt)=>{
     evt.preventDefault();
-    const aAnswers = ["no", "yes"];
-    const nAnswer = Math.floor(Math.random() * aAnswers.length);
-    document.getElementById("answer").innerHTML = aAnswers[nAnswer];
+    const sTodo = document.getElementById("todoEntry").value;
+    addTodo(sTodo);
 });
