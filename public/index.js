08ceb0e5a27e7eb6919f23816ce4777dbe86e7f1
Rich Hildred <rhildred@gmail.com>
2020-10-07 13:06:43 -0400

    simplified

--- public/index.js
+++ public/index.js
@@ -1,7 +1,4 @@
 // Libraries
-import config from "./firebase.js";
-import "https://cdnjs.cloudflare.com/ajax/libs/firebase/7.16.0/firebase-app.min.js";
-import "https://cdnjs.cloudflare.com/ajax/libs/firebase/7.16.0/firebase-database.min.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/js/framework7.min.js";
 
 // here initialize the app
@@ -17,55 +14,20 @@ myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
-firebase.initializeApp(config);
-
 function addTodo(sKey, sTodo){
   let oTodo = document.createElement("p");
-  oTodo.innerHTML = sTodo + "<div class=\"row\"><div class=\"col\"><button id=\"d" + sKey + "\" class=\"delete\">delete</button></div><div class=\"col\"><button id=\"f" + sKey + "\" class=\"finish\">finish</button></div></div>";
+  oTodo.innerHTML = `<span id="${sKey}" class="row">${sTodo}</span>`;
   document.getElementById("list").prepend(oTodo);
 }
 
-firebase.database().ref("todos").on("value", (snapshot) =>{
-  const oTodos=snapshot.val();
-  console.log(oTodos);
-  document.getElementById("list").innerHTML = "";
-  let aTodos = Object.keys(oTodos);
-  for(let n = 0; n < aTodos.length; n++){
-    const sKey = aTodos[n];
-    addTodo(sKey, oTodos[aTodos[n]].name);
-  }
-  createDeleteHandlers();
-  createFinishHandlers();
-})
 
 document.getElementById("todo").addEventListener("submit", (evt)=>{
     evt.preventDefault();
     const sTodo = document.getElementById("todoEntry").value;
-    const sId = new Date().toISOString().replace(".", "_");
-    firebase.database().ref("todos/" + sId).set({
-      name: sTodo
-    });
+    if(sTodo){
+      const sId = new Date().toISOString().replace(".", "_");
+      addTodo(sId, sTodo);
+      document.getElementById("todoEntry").value = "";
+    }
 });
 
-function createDeleteHandlers(){
-  var aClassname = document.getElementsByClassName("delete");
-
-  for(var n = 0; n < aClassname.length; n++){
-    aClassname[n].addEventListener("click", (evt) =>{
-      const sId = evt.target.id.substr(1); 
-      firebase.database().ref("todos/" + sId).remove();
-    })
-  }
-}
-
-function createFinishHandlers(){
-  var aClassname = document.getElementsByClassName("finish");
-
-  for(var n = 0; n < aClassname.length; n++){
-    aClassname[n].addEventListener("click", (evt) =>{
-      const sId = evt.target.id.substr(1);
-      const sFinished =  new Date().toISOString().replace(".", "_");
-      firebase.database().ref("todos/" + sId + "/completed").set(sFinished);
-    })
-  }
-}
